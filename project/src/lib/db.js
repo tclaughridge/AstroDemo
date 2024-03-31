e25e25e2118f4d44d40fb3ecac1387c0cd6291c0
tclaughridge <tcl5tu@virginia.edu>
2024-03-31 17:48:41 -0400

    Corrected DB credentials

--- project/src/lib/db.js
+++ project/src/lib/db.js
@@ -1,4 +1,7 @@
 import neo4j from 'neo4j-driver';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
@@ -8,3 +11,31 @@ const driver = neo4j.driver(
 export const getSession = () => {
   return driver.session();
 };
+
+export async function testDbConnection() {
+  const session = driver.session();
+
+  try {
+    const result = await session.run('RETURN "" as message');
+    const singleRecord = result.records[0];
+    const message = singleRecord.get('message');
+
+    console.log(message);
+    return true;
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    return false;
+  } finally {
+    await session.close();
+  }
+}
+
+// testDbConnection()
+//   .then((isConnected) => {
+//     if (isConnected) {
+//       console.log("Database connection is successful!");
+//     } else {
+//       console.log("Database connection failed.");
+//     }
+//   });
+
